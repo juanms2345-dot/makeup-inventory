a180a2c086772d46287bda6e3bfcb12d02860ccf
juanms2345-dot <juanms2345@gmail.com>
2026-09-09 13:22:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 // Variables de entorno de Supabase
